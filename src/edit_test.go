7475e70ceeaccf1da6727158fbc2e8c7aac2c4d9
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2017-11-23 16:01:37 +0100

    renamed test files and improved test output

--- src/edit_test.go
+++ src/edit_test.go
@@ -6,6 +6,8 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/mitchellh/cli"
 )
 
 func TestEdit(t *testing.T) {
@@ -15,16 +17,35 @@ func TestEdit(t *testing.T) {
 		fmt.Fprintln(os.Stderr, err.Error())
 	}
 
+	ui := cli.NewMockUi()
+	c := &EditCommand{Ui: ui}
+
 	t.Run("ParseInvalidSecretFile", func(t *testing.T) {
 
 		test_files := map[string]string{
-			"invalid_secret1.txt": "Unable to parse key/value pair \"valid_key: invalid: _value\". Make sure that there is only/at least one \":\" in it",
-			"invalid_secret2.txt": "Unable to parse key/value pair \"invalid_line\". Make sure that there is only/at least one \":\" in it",
+			"invalid_secret_multiple_delimiters.txt": "Unable to parse key/value pair \"valid_key: invalid: _value\". Make sure that there is only/at least one \":\" in it",
+			"invalid_secret_missing_delimiter.txt":   "Unable to parse key/value pair \"invalid_line\". Make sure that there is only/at least one \":\" in it",
 		}
 
 		for file, expected := range test_files {
 
-			_, actual := ParseSecret("../sample/tests/secrets/" + file)
+			_, actual := c.ParseSecret("../sample/tests/secrets/" + file)
+
+			if !strings.Contains(actual.Error(), expected) {
+				t.Fatalf("\nexpected:\t%s\nto include:\t%s", actual, expected)
+			}
+		}
+	})
+
+	t.Run("ParseSecretFileWithDuplicateKey", func(t *testing.T) {
+
+		test_files := map[string]string{
+			"invalid_secret_duplicated_key.txt": "Secret identifier \"duplicate_key\" is used multiple times. Please make sure that the key only is used once.",
+		}
+
+		for file, expected := range test_files {
+
+			_, actual := c.ParseSecret("../sample/tests/secrets/" + file)
 
 			if !strings.Contains(actual.Error(), expected) {
 				t.Fatalf("\nexpected:\t%s\nto include:\t%s", actual, expected)
@@ -45,13 +66,14 @@ func TestEdit(t *testing.T) {
 
 		for file, expected := range test_files {
 
-			actual, err := ParseSecret("../sample/tests/secrets/" + file)
+			actual, err := c.ParseSecret("../sample/tests/secrets/" + file)
 			if err != nil {
 				t.Fatalf("Unable to parse secret file: %q", err)
 			}
 
 			if !reflect.DeepEqual(actual, expected) {
-				t.Fatalf("\nexpected:\n%q\n\nto include:\n%q", expected, actual)
+				t.Fatalf("\nexpected:\t%s\nto include:\t%s", actual, expected)
+				t.Fatalf("\nexpected:\n%q\n\nto include:\n%q", actual, expected)
 			}
 		}
 	})
