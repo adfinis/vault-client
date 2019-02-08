e2931405d48fd8c06f98c8c0e912cbf38a82bb12
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-02-08 16:14:38 +0100

    Add KvClient skeleton

---
+++ src/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	vault "github.com/hashicorp/vault/api"
+)
+
+type KvClient interface {
+	Put(key string, value map[string]interface{}) error
+	Get(key string) (map[string]interface{}, error)
+	Delete(key string) error
+	List(key string) ([]string, error)
+}
+
+type KvClientV1 struct {
+	Client *vault.Client
+}
+
+func NewKvClientV1(cfg *vault.Config, token string) (*KvClientV1, error) {
+	c, err := vault.NewClient(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	c.SetToken(token)
+	c.Auth()
+
+	return &KvClientV1{Client: c}, nil
+}
+
+func (c *KvClientV1) Put(key string, value map[string]interface{}) error { return nil }
+
+func (c *KvClientV1) Get(key string) (map[string]interface{}, error) { return nil, nil }
+
+func (c *KvClientV1) Delete(key string) error { return nil }
+
+func (c *KvClientV1) List(key string) ([]string, error) { return nil, nil }
