e9e15e88cca7b4acfbbab87be276d0001b7f89da
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-02-13 18:06:50 +0100

    Implement List() and Traverse() on KvClient

--- src/client.go
+++ src/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	vault "github.com/hashicorp/vault/api"
+	"strings"
 )
 
 type KvClient interface {
@@ -10,6 +11,7 @@ type KvClient interface {
 	Get(key string) (map[string]interface{}, error)
 	Delete(key string) error
 	List(key string) ([]string, error)
+	Traverse(key string) ([]string, error)
 	RawClient() *vault.Client
 }
 
@@ -59,6 +61,91 @@ func (c *KvClientV1) Delete(key string) error {
 	return nil
 }
 
-func (c *KvClientV1) List(key string) ([]string, error) { return nil, nil }
+func (c *KvClientV1) Traverse(key string) ([]string, error) {
+	childs, err := c.List(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(childs) == 0 {
+		return nil, nil
+	}
+
+	var paths []string
+
+	for _, child := range childs {
+		// Prefix child with path
+		child = fmt.Sprint(key, child)
+		if strings.HasSuffix(child, "/") {
+			childs, err := c.Traverse(child)
+			if err != nil {
+				return nil, err
+			}
+			paths = append(paths, childs...)
+		} else {
+			paths = append(paths, child)
+		}
+	}
+	return paths, nil
+}
+
+func (c *KvClientV1) List(key string) ([]string, error) {
+
+	key = strings.TrimLeft(key, "/")
+	if key == "" {
+		return kvMounts(c)
+	}
+
+	secret, err := c.client.Logical().List(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if secret == nil {
+
+		// Key could be an empty backend
+		var tmp string
+		if !strings.HasPrefix("/", key) {
+			tmp = key + "/"
+		}
+
+		mounts, err := kvMounts(c)
+		if err != nil {
+			return nil, err
+		}
+		for _, k := range mounts {
+			if tmp == k {
+				return []string{k}, nil
+			}
+		}
+
+		// Or it could simply not exist...
+		return nil, fmt.Errorf("Secret does not exist")
+	}
+
+	var childs []string
+	for _, path := range secret.Data {
+		childs = strings.Split(strings.Trim(fmt.Sprint(path), "[]"), " ")
+	}
+
+	return childs, nil
+
+}
 
 func (c *KvClientV1) RawClient() *vault.Client { return c.client }
+
+// Return a list of kv engines
+func kvMounts(c *KvClientV1) ([]string, error) {
+	mounts, err := c.client.Sys().ListMounts()
+	if err != nil {
+		return nil, err
+	}
+
+	var backends []string
+	for x, i := range mounts {
+		if i.Type == "kv" {
+			backends = append(backends, x)
+		}
+	}
+	return backends, nil
+}
