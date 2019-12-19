1ab31bce2f2c31d5a7b46d1a68c3f6d388140028
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-12-19 10:59:53 +0100

    Allow to load .vaultrc from different locations

--- src/config.py
+++ src/config.py
@@ -1,6 +1,24 @@
 import yaml
 
+from os import getenv
+from os.path import exists
+
+_DEFAULT_CONFIG = {
+    'host': 'localhost',
+    'port': 8200,
+    'tls': True,
+    'verify_tls': True
+}
 
 def load_config():
-    with open(".vaultrc", "r") as f:
-        return yaml.load(f, Loader=yaml.FullLoader)
+    path = config_path()
+    with open(path, "r") as f:
+        return  {**_DEFAULT_CONFIG, **yaml.load(f, Loader=yaml.FullLoader)}
+
+def config_path():
+    envvar = getenv('VC_CONFIG')
+    if envvar:
+        return envvar
+    elif exists('.vaultrc'):
+        return '.vaultrc'
+    return expanduser('~/.vaultrc')
