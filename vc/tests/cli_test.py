757b67e4097b0962648592aa35e9528124264adb
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-12-19 16:23:58 +0100

    Add more command tests

--- vc/tests/cli_test.py
+++ vc/tests/cli_test.py
@@ -2,6 +2,10 @@ import pytest
 import re
 
 
+# TODO: Test search command
+# TODO: Test login command
+
+
 @pytest.mark.parametrize(
     "backend", [pytest.lazy_fixture("v1_backend"), pytest.lazy_fixture("v2_backend")]
 )
@@ -24,12 +28,50 @@ def test_show(run_cmd, backend, path, rc, output):
 @pytest.mark.parametrize(
     "path,rc,output",
     [
-        ("", 0, "secret0\nsecret1\nsecret2\nsecret3\nsecretdir/\n"),
+        ("secret1", 0, "Secret successfully deleted\n"),
         ("nonexistent", 1, 'Path ".*" does not exist.\n'),
     ],
 )
-def test_list(run_cmd, backend, path, rc, output):
-    result = run_cmd(["list", f"{backend}/{path}"])
+def test_delete(run_cmd, backend, path, rc, output):
+    result = run_cmd(["delete", f"{backend}/{path}"])
+    assert re.match(output, result.output)
+    assert result.exit_code == rc
+
+
+@pytest.mark.parametrize(
+    "backend", [pytest.lazy_fixture("v1_backend"), pytest.lazy_fixture("v2_backend")]
+)
+@pytest.mark.parametrize(
+    "path,rc,output",
+    [
+        # TODO: Should we mock click.edit()?
+        ("nonexistent", 1, "Path .* does not yet exist. Creating a new secret.\n")
+    ],
+)
+def test_edit(run_cmd, backend, path, rc, output):
+    result = run_cmd(["edit", f"{backend}/{path}"])
+    assert re.match(output, result.output)
+    assert result.exit_code == rc
+
+
+@pytest.mark.parametrize(
+    "backend", [pytest.lazy_fixture("v1_backend"), pytest.lazy_fixture("v2_backend")]
+)
+@pytest.mark.parametrize(
+    "path,opts,rc,output",
+    [
+        ("", [], 0, "secret0\nsecret1\nsecret2\nsecret3\nsecretdir/\n"),
+        ("nonexistent", [], 1, 'Path ".*" does not exist.\n'),
+        (
+            "",
+            ["-r"],
+            0,
+            "test/v[1,2]/secret0\ntest/v[1,2]/secret[1,2]\ntest/v[1,2]/secret2\ntest/v[1,2]/secret3\ntest/v[1,2]/secretdir/subsecret0\ntest/v[1,2]/secretdir/subsecret[1,2]\ntest/v[1,2]/secretdir/subsecret2\ntest/v[1,2]/secretdir/subsecret3\n",
+        ),
+    ],
+)
+def test_list(run_cmd, backend, path, opts, rc, output):
+    result = run_cmd(["list", *opts, f"{backend}/{path}"])
     assert re.match(output, result.output)
     assert result.exit_code == rc
 
@@ -42,10 +84,45 @@ def test_list(run_cmd, backend, path, rc, output):
     [
         ("secret1", "key=value", 0, "Secret successfully inserted!\n"),
         ("secret1", "key=key=value", 1, "Data .* is not a valid key/value pair.\n"),
+        ("secret1", "key", 1, "Data .* is not a valid key/value pair.\n"),
     ],
 )
 def test_insert(run_cmd, backend, path, data, rc, output):
     result = run_cmd(["insert", f"{backend}/{path}", data])
+    assert re.match(output, result.output)
+    assert result.exit_code == rc
+
+
+@pytest.mark.parametrize(
+    "backend", [pytest.lazy_fixture("v1_backend"), pytest.lazy_fixture("v2_backend")]
+)
+@pytest.mark.parametrize(
+    "src,dest,rc,output",
+    [
+        ("secret1", "newsecret", 0, "Secret successfully copied!\n"),
+        ("nonexistent", "newsecret", 1, "Source path .* does not exist.\n"),
+        ("secret1", "secret2", 1, "The destination secret already exists..*"),
+    ],
+)
+def test_copy(run_cmd, backend, src, dest, rc, output):
+    result = run_cmd(["cp", f"{backend}/{src}", f"{backend}/{dest}"])
+    assert re.match(output, result.output)
+    assert result.exit_code == rc
+
+
+@pytest.mark.parametrize(
+    "backend", [pytest.lazy_fixture("v1_backend"), pytest.lazy_fixture("v2_backend")]
+)
+@pytest.mark.parametrize(
+    "src,dest,rc,output",
+    [
+        ("secret1", "newsecret", 0, "Secret successfully copied!\n"),
+        ("nonexistent", "newsecret", 1, "Source path .* does not exist.\n"),
+        ("secret1", "secret2", 1, "The destination secret already exists..*"),
+    ],
+)
+def test_move(run_cmd, backend, src, dest, rc, output):
+    result = run_cmd(["cp", f"{backend}/{src}", f"{backend}/{dest}"])
     assert re.match(output, result.output)
     assert result.exit_code == rc
 
