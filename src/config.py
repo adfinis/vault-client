4f18e37292511ba60b1c1c6cac0c8931842f9a50
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-12-19 11:00:31 +0100

    Add function to update token in config file

--- src/config.py
+++ src/config.py
@@ -22,3 +22,18 @@ def config_path():
     elif exists('.vaultrc'):
         return '.vaultrc'
     return expanduser('~/.vaultrc')
+
+def update_config_token(token):
+    path = config_path()
+    with open(path, "r") as f:
+        config = f.readlines()
+
+    updated = []
+    for line in config:
+        if line.startswith('token:'):
+            updated += f"token: {token}\n"
+            continue
+        updated += line
+
+    with open(path, "w") as f:
+        f.writelines(updated)
