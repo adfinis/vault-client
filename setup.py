a797f003b7f64bffae39f32ca96ec3cec976b9c7
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2020-01-08 14:59:39 +0100

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vc",
+    version="2.0.0",
+    url="https://github.com/adfinis-sygroup/vault-client.git",
+    author="Patrick Winter",
+    author_email="info@adfinis-sygroup.ch",
+    description="A command-line interface to HashiCorp's Vault",
+    packages=find_packages(),
+    install_requires=[
+        "PyYAML >= 5.2",
+        "Click >= 7.0",
+        "hvac >= 0.9.6",
+        "future-fstrings",
+    ],
+)
