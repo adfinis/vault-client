681ab90bf000ab8e33633a3895f0ef9052867cd5
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2020-01-24 14:00:46 +0100

    Improve dynamic candidates for bash autcompletion

--- sample/vc-completion.bash
+++ sample/vc-completion.bash
@@ -1,17 +1,28 @@
-# completion file for bash
+#/usr/bin/env bash
 
 _vc()
 {
     COMREPLY=()
-    local cur=${COMP_WORDS[COMP_CWORD]}
+    local CUR=${COMP_WORDS[COMP_CWORD]}
     if [[ $COMP_CWORD -gt 1 ]]; then
 	case "${COMP_WORDS[1]}" in
 	    show|insert|edit|rm|ls|cp|mv)
-		COMPREPLY=($(compgen -W "$(vc ls -r)" -- ${cur}))
-		;;
+
+		local DIR
+		if [[ -n ${CUR} ]]; then
+		    # Remove everything after the last slash
+		    BASE=$(echo $CUR | sed 's/\(.*\/\).*$/\1/')
+		else
+		    BASE=""
+		fi
+
+		COMPREPLY=(
+		    $(compgen -W "$(vc ls -a ${BASE})" ${CUR})
+		)
+		compopt -o nospace
 	esac
     else
-        COMPREPLY=($(compgen -W "login show insert cp mv rm edit ls" -- $cur))
+        COMPREPLY=($(compgen -W "login show insert cp mv rm edit ls" -- $CUR))
     fi
 }
 complete -F _vc vc
